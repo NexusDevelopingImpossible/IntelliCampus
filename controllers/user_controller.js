4d0dfa92b02c20fc67f28fca9a1f9692935ec7d2
Rajsekhor Saikia <rajsekhor.saikia.2001@gmail.com>
2023-06-01 12:52:25 +0530

    v0.0.10: Passport-Local User Authentication added

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,5 +1,97 @@
+const User = require('../models/user');
+
 module.exports.Login = function(req, res){
     return res.render('Login', {
         title: "Login"
     });
 }
+
+module.exports.signUp=(req,res)=>{
+    return res.render('signup',{
+        title:"Sign Up"
+    })
+}
+
+module.exports.temp=(req,res)=>{
+    return res.render('tempView',{
+        title:"Test"
+    })
+}
+
+module.exports.student=(req,res)=>{
+    return res.render('student',{
+        title:"Test"
+    })
+}
+
+module.exports.teacher=(req,res)=>{
+    return res.render('teacher',{
+        title:"Test"
+    })
+}
+
+module.exports.admin=(req,res)=>{
+    return res.render('admin',{
+        title:"Test"
+    })
+}
+
+module.exports.create = (req, res) => {
+    if (req.body.password != req.body.confirm_password) {
+        return res.redirect('back');
+    }
+    User.findOne({ username: req.body.username }, (err, user) => {
+        if (err) { console.log('error in finding user'); return; }
+        if (!user) {
+            User.create(req.body, (err, user) => {
+                if (err) { console.log('error in finding user ' ,err); return; }
+
+                res.redirect('/users/Login');
+            });
+        }
+        else {
+            res.redirect('back');
+        }
+    });
+};
+
+
+
+module.exports.createSession = (req, res) => {
+    console.log(req.body);
+    const { username, password } = req.body;
+
+    // Find the user in the database based on the provided username
+    User.findOne({ username: username }, (err, user) => {
+        if (err) {
+            console.log('Error:', err);
+            return res.redirect('/');
+        }
+
+        if (!user || user.password !== password) {
+            // User not found or password is incorrect
+            return res.redirect('/');
+        }
+        // Redirect to different routes based on the user's type
+        if (user.position === 'student') {
+            return res.redirect('/users/student');
+        } else if (user.position === 'teacher') {
+            return res.redirect('/users/teacher');
+        } else if (user.position === 'admin') {
+            return res.redirect('/users/admin');
+        } else {
+            return res.redirect('/users/signup');
+        }
+    });
+};
+
+
+module.exports.destroySession = function (req, res) {
+    // logout has been upgraded as an asynchronous function so it requires a callback function to handle error now
+    req.logout(function (error) {
+      if (error) {
+        return next(error);
+      }
+      return res.redirect("/");
+    });
+  };
--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const sassMiddleware = require('node-sass-middleware');
+const session=require('express-session');
+const passport=require('passport');
+const passportLocal=require('./config/passport-local-strategy');
+const cookieParser = require('cookie-parser');
+const MongoStore = require("connect-mongo");
+const db = require('./config/mongoose');
+
+app.use(cookieParser());
 
 app.use(sassMiddleware({
     src: './assets/scss',
@@ -21,6 +29,31 @@ app.set('layout extractStyles', true);
 app.set('layout extractScript', true);
 
 app.use(expressLayouts);
+
+app.use(session({
+  name: 'quiklink',
+  //TODO change secretbefore deployment 
+  secret: 'blahsomething',
+  saveUninitialized: false,
+  resave: false,
+  cookie: {
+      maxAge: (1000 * 60 * 100)
+  },
+  store: MongoStore.create({
+      mongooseConnection: db,
+      autoRemove: "disabled",
+      // Added next line from stackoverflow to remove the (session) parameter from line 13
+      mongoUrl: 'mongodb://localhost/quiklink'
+    }),function(error){
+      console.log(error || 'connect-mongodb setup okay');
+    }
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
 
 app.use('/', require('./routes'));
 
