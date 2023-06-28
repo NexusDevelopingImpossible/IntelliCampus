22f30dc274fed7127757f440f6788602db1ed90e
Om Sinkar <omsinkar03bit@gmail.com>
2023-06-28 17:31:06 +0530

    v0.11.0: MongoDB Atlas added.

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,16 +1,33 @@
 const mongoose = require('mongoose');
-const env = require('./environment');
+// const env = require('./environment');
 
-mongoose.connect("mongodb://localhost/ECM");
+// mongoose.connect("mongodb://localhost/ECM");
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
-
-
-db.once('open', function () {
-    console.log("Connected to Database :: MongoDB/");
-});
+// db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
 
 
-module.exports = db;
+// db.once('open', function () {
+//     console.log("Connected to Database :: MongoDB/");
+// });
+
+
+// module.exports = db;
+
+
+
+const uri = "mongodb+srv://omsinkar03bit:WBl5pvvuWTQmeHS9@intellicampus.yc6cf9z.mongodb.net/?retryWrites=true&w=majority";
+mongoose.set('strictQuery', false);
+
+const start = async() => {
+    try{
+        await mongoose.connect(uri);
+
+    }
+    catch(err){
+        console.log(err); 
+    }
+
+}
+start()
--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(cookieParser());
 
 app.use(express.urlencoded());
 console.log(__dirname + '\\public\\assets\\')
-app.use(express.static(__dirname + '\\public\\assets\\'));
+app.use(express.static(__dirname + '/public/assets/'));
 // app.use(express.static(__dirname + './assets'));
 // app.use(express.static('./assets'));
 
