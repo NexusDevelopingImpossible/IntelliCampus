33a3d298d04380c11a0bb9fd323b265ed5bd3790
Om Sinkar <omsinkar03bit@gmail.com>
2023-07-21 12:21:18 +0530

    v0.28.11: Controller and route added for exam cell pages.

--- controllers/examcell_controller.js
+++ controllers/examcell_controller.js
@@ -9,3 +9,35 @@ module.exports.dashboard = async (req, res) => {
     console.log(err);
   }
 };
+module.exports.cform = async (req, res) => {
+  try {
+    checkurlfunct.checkurlexamcell(req, res);
+    res.render("examcell/cform", { title: "C Form"});
+  } catch (err) {
+    console.log(err);
+  }
+};
+module.exports.cgpa = async (req, res) => {
+  try {
+    checkurlfunct.checkurlexamcell(req, res);
+    res.render("examcell/cgpabacklog", { title: "CGPA & Backlog"});
+  } catch (err) {
+    console.log(err);
+  }
+};
+module.exports.createsubject = async (req, res) => {
+  try {
+    checkurlfunct.checkurlexamcell(req, res);
+    res.render("examcell/createsubject", { title: "Create Subject"});
+  } catch (err) {
+    console.log(err);
+  }
+};
+module.exports.lockinternal = async (req, res) => {
+  try {
+    checkurlfunct.checkurlexamcell(req, res);
+    res.render("examcell/lockinternal", { title: "Lock internal"});
+  } catch (err) {
+    console.log(err);
+  }
+};
--- routes/examcell.js
+++ routes/examcell.js
@@ -6,5 +6,9 @@ const router = express.Router();
 const examcellController = require('../controllers/examcell_controller');
 
 router.get("/dashboard", examcellController.dashboard);
+router.get("/cgpa", examcellController.cgpa);
+router.get("/lockinternal", examcellController.lockinternal);
+router.get("/createsubject", examcellController.createsubject);
+router.get("/cform", examcellController.cform);
 
 module.exports = router;
