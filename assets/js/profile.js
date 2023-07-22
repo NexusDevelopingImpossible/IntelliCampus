0d6636eefe79702c891a8f68e7ac4342cdb5e4c6
srijandev1 <srijandev4@gmail.com>
2023-07-22 16:30:07 +0530

    v0.28.17: Profile page updated

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -1,9 +1,13 @@
 function start(){
   "use strict";
   const editBtn = document.querySelector(".edit-btn");
-const inputEdit = document.querySelectorAll('input[type="text"], textarea[type="text"], select');
+const inputEdit = document.querySelectorAll('input[type="text"], textarea[type="text"], select, input[type="date"]');
+const dpEdit = document.querySelectorAll('#file-upload');
+const docEdit = document.querySelectorAll('#select-file');
+const downloadBtn = document.getElementById('download-btn');
 const selectEdit = document.querySelectorAll('select');
-const saveBtn = document.querySelector(".popup-btns");
+const customUpload = document.querySelectorAll(".custom-file-upload");
+const saveBtn = document.getElementById("save-btn");
 
 editBtn.addEventListener("click", function () {
 
@@ -11,17 +15,38 @@ editBtn.addEventListener("click", function () {
     input.classList.toggle("input-edit");
     input.readOnly = !input.readOnly;
   });
+  docEdit.forEach(function (input) {
+    input.disabled = !input.disabled;
+  });
+  dpEdit.forEach(function (input) {
+    input.disabled = !input.disabled;
+  });
+
   selectEdit.forEach(function (select) {
     select.disabled = !select.disabled;
 
   });
+ 
+
   editBtn.classList.toggle("edit-btn-clicked");
 //   selectEdit.disabled = false;
 
 if (editBtn.classList.contains("edit-btn-clicked")) {
     saveBtn.style.display = "block";
+    customUpload.forEach(function(doc){
+      doc.classList.remove("input-read");
+      doc.classList.add("input-edit");
+      downloadBtn.style.display = "none";
+    });
   } else {
     saveBtn.style.display = "none";
+    customUpload.forEach(function(doc){
+      doc.classList.add("input-read");
+      doc.classList.remove("input-edit");
+      downloadBtn.style.display = "block";
+    });
+    // dpEdit.addAttribute("disabled")
+    // docEdit.addAttribute("disabled");
   }
 
 
