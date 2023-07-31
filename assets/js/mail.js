206fd59471794f7e1ad92bcbd37e39724a57ff53
srijandev1 <srijandev4@gmail.com>
2023-07-31 22:59:56 +0530

    v0.30.20: Internal Marks, Mail, report css fixed

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,38 +1,9 @@
-// const modal = document.getElementById("modal");
-// const overlay = document.getElementById("overlay");
-// const composeButton = document.getElementById("compose__btn");
-// const closeButton = document.getElementById("close");
-
-// // adding the show class
-// composeButton.addEventListener('click', () => {
-//     modal.classList.add('show');
-//     overlay.classList.add('show')
-// });
-
-// closeButton.addEventListener('click' ,() => {
-//     modal.classList.remove('show');
-//     overlay.classList.remove('show')
-// });
-
-// overlay.addEventListener('click' ,() => {
-//     modal.classList.remove('show');
-//     overlay.classList.remove('show')
-// });
 
 const composeButton = document.getElementById("compose__btn");
-const popOpen = document.querySelectorAll(".mail");
-const cross1 = document.querySelectorAll(".close");
 const cross = document.getElementById("close");
 const popup = document.querySelector(".popup");
 const modal = document.querySelector(".modal");
 
-let count = 0;
-
-function reportOpen(id) {
-  console.log(id);
-  document.getElementById(`pop${id}`).style.display = "flex";
-  document.getElementById(`modal${id}`).style.display = "block";
-}
 
 composeButton.addEventListener("click", function () {
   popup.style.display = "flex";
@@ -43,9 +14,3 @@ cross.addEventListener("click", function () {
   modal.style.display = "none";
 });
 
-cross1.forEach((cross) => {
-  cross.addEventListener("click", function () {
-    popup.style.display = "none";
-    modal.style.display = "none";
-  });
-});
